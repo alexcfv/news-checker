4f90846824aa55830026e84ef31379af3eb4e927
Kramsloi Alexej <109853627+alexcfv@users.noreply.github.com>
2025-06-07 20:37:23 +0200

    version 1 update
    add constants file

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,2 @@
-from .entites.config import api_key, logging_level, start_message, error_message
+from .entites.config import api_key, logging_level
+from .entites.constants import start_message, error_message
